05b6692685eda11cb74ee40820b246b07961072a
Konstantin Aleshkin <kkcnst@gmail.com>
2021-07-12 09:27:56 -0400

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,23 +1,15 @@
-{ mkDerivation, base, bytestring, containers, exceptions, gl, HUnit
-, lens, linear, mtl, sdl2, semigroups, stdenv, test-framework
-, test-framework-hunit, test-framework-quickcheck2, text
-, transformers, vector
+{ mkDerivation, base, bytestring, containers, exceptions, gl, lens
+, lib, linear, mtl, semigroups, text, transformers, vector
 }:
 mkDerivation {
   pname = "caramia";
-  version = "0.7.2.2";
+  version = "0.7.2.3";
   src = ./.;
-  isLibrary = true;
-  isExecutable = true;
   libraryHaskellDepends = [
     base bytestring containers exceptions gl lens linear mtl semigroups
     text transformers vector
   ];
-  testHaskellDepends = [
-    base containers HUnit linear sdl2 test-framework
-    test-framework-hunit test-framework-quickcheck2 transformers
-  ];
   homepage = "https://github.com/Noeda/caramia/";
   description = "High-level OpenGL bindings";
-  license = stdenv.lib.licenses.mit;
+  license = lib.licenses.mit;
 }
